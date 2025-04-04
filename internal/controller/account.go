7b0d14eaf3ede799f76234b7a5e3896997b9e345
kodaimura <kodaimura123@gmail.com>
2025-04-05 00:12:57 +0900

    copy from goat

---
+++ internal/controller/account.go
@@ -0,0 +1,157 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"goscaf/pkg/jwt"
+	"goscaf/pkg/utils"
+	"goscaf/internal/service"
+	"goscaf/internal/dto"
+	"goscaf/internal/request"
+)
+
+type AccountController struct {
+	accountService service.AccountService
+}
+
+func NewAccountController() *AccountController {
+	return &AccountController{
+		accountService: service.NewAccountService(),
+	}
+}
+
+// GET /signup
+func (ctr *AccountController) SignupPage(c *gin.Context) {
+	c.HTML(200, "signup.html", gin.H{})
+}
+
+// GET /login
+func (ctr *AccountController) LoginPage(c *gin.Context) {
+	c.HTML(200, "login.html", gin.H{})
+}
+
+// GET /logout
+func (ctr *AccountController) Logout(c *gin.Context) {
+	jwt.RemoveTokenFromCookie(c)
+	c.Redirect(303, "/login")
+}
+
+// POST /api/signup
+func (ctr *AccountController) ApiSignup(c *gin.Context) {
+	var req request.Signup
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(errs.NewBadRequestError(err.Error()))
+		return
+	}
+
+	var input dto.Signup
+	utils.MapFields(&input, req)
+
+	result, err := ctr.accountService.Signup(input)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, result)
+}
+
+// POST /api/login
+func (ctr *AccountController) ApiLogin(c *gin.Context) {
+	var req request.Login
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(errs.NewBadRequestError(err.Error()))
+		return
+	}
+
+	var input dto.Login
+	utils.MapFields(&input, req)
+
+	account, err := ctr.accountService.Login(input)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	pl, err := ctr.accountService.GenerateJwtPayload(dto.AccountPK{Id: account.Id})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	jwt.SetTokenToCookie(c, pl)
+	c.JSON(200, gin.H{})
+}
+
+// GET /api/accounts/me
+func (ctr *AccountController) ApiGetOne(c *gin.Context) {
+	pl := jwt.GetPayload(c)
+	result, err := ctr.accountService.GetOne(dto.AccountPK{Id: pl.AccountId})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, result)
+}
+
+// PUT /api/accounts/me/password
+func (ctr *AccountController) ApiPutPassword(c *gin.Context) {
+	pl := jwt.GetPayload(c)
+
+	var req request.PutAccountPassword
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(errs.NewBadRequestError(err.Error()))
+		return
+	}
+
+	_, err := ctr.accountService.Login(dto.Login{Name: pl.AccountName, Password: req.OldPassword})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	var input dto.UpdateAccount
+	utils.MapFields(&input, req)
+	input.Id = pl.AccountId
+
+	if err := ctr.accountService.Update(input); err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, gin.H{})
+}
+
+// PUT /api/accounts/me/name
+func (ctr *AccountController) ApiPutName(c *gin.Context) {
+	pl := jwt.GetPayload(c)
+
+	var req request.PutAccountName
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(errs.NewBadRequestError(err.Error()))
+		return
+	}
+
+	var input dto.UpdateAccount
+	utils.MapFields(&input, req)
+	input.Id = pl.AccountId
+
+	if err := ctr.accountService.Update(input); err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, gin.H{})
+}
+
+// DELETE /api/accounts/me
+func (ctr *AccountController) ApiDelete(c *gin.Context) {
+	pl := jwt.GetPayload(c)
+	if err := ctr.accountService.Delete(dto.AccountPK{Id: pl.AccountId}); err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, gin.H{})
+}
---
+++ internal/service/account.go
@@ -0,0 +1,148 @@
+package service
+
+import (
+	"database/sql"
+	"golang.org/x/crypto/bcrypt"
+
+	"goscaf/internal/core/jwt"
+	"goscaf/internal/core/logger"
+	"goscaf/internal/core/errs"
+	"goscaf/internal/core/utils"
+	"goscaf/internal/dto"
+	"goscaf/internal/model"
+	"goscaf/internal/repository"
+)
+
+type AccountService interface {
+	GetOne(input dto.AccountPK) (dto.Account, error)
+	Delete(input dto.AccountPK) error
+	Update(input dto.UpdateAccount) error
+	Login(input dto.Login) (dto.Account, error)
+	Signup(input dto.Signup) (dto.AccountPK, error)
+	GenerateJwtPayload(input dto.AccountPK) (jwt.Payload, error)
+}
+
+type accountService struct {
+	accountRepository repository.AccountRepository
+}
+
+func NewAccountService() AccountService {
+	return &accountService{
+		accountRepository: repository.NewAccountRepository(),
+	}
+}
+
+func (srv *accountService) GetOne(input dto.AccountPK) (dto.Account, error) {
+	account, err := srv.accountRepository.GetOne(&model.Account{Id: input.Id})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return dto.Account{}, errs.NewNotFoundError()
+		}
+		logger.Error(err.Error())
+		return dto.Account{}, errs.NewUnexpectedError(err.Error())
+	}
+
+	var ret dto.Account
+	utils.MapFields(&ret, account)
+	return ret, nil
+}
+
+func (srv *accountService) Update(input dto.UpdateAccount) error {
+	account, err := srv.accountRepository.GetOne(&model.Account{Id: input.Id})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return errs.NewNotFoundError()
+		}
+		logger.Error(err.Error())
+		return errs.NewUnexpectedError(err.Error())
+	}
+
+	if input.Name != "" {
+		account.Name = input.Name
+	}
+	if input.Password != "" {
+		hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+		if err != nil {
+			logger.Error(err.Error())
+			return errs.NewUnexpectedError(err.Error())
+		}
+		account.Password = string(hashed)
+	}
+	if err := srv.accountRepository.Update(&account, nil); err != nil {
+		if column, ok := GetConflictColumn(err); ok {
+			return errs.NewConflictError(column)
+		}
+		logger.Error(err.Error())
+		return errs.NewUnexpectedError(err.Error())
+	}
+	return nil
+}
+
+func (srv *accountService) Delete(input dto.AccountPK) error {
+	if err := srv.accountRepository.Delete(&model.Account{Id: input.Id}, nil); err != nil {
+		logger.Error(err.Error())
+		return errs.NewUnexpectedError(err.Error())
+	}
+	return nil
+}
+
+func (srv *accountService) Login(input dto.Login) (dto.Account, error) {
+	account, err := srv.accountRepository.GetOne(&model.Account{Name: input.Name})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return dto.Account{}, errs.NewUnauthorizedError()
+		}
+		logger.Error(err.Error())
+		return dto.Account{}, errs.NewUnexpectedError(err.Error())
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(input.Password)); err != nil {
+		return dto.Account{}, errs.NewUnauthorizedError()
+	}
+
+	var ret dto.Account
+	utils.MapFields(&ret, account)
+	return ret, nil
+}
+
+func (srv *accountService) Signup(input dto.Signup) (dto.AccountPK, error) {
+	if _, err := srv.accountRepository.GetOne(&model.Account{Name: input.Name}); err == nil {
+		return dto.AccountPK{}, errs.NewConflictError("account_name")
+	}
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		logger.Error(err.Error())
+		return dto.AccountPK{}, errs.NewUnexpectedError(err.Error())
+	}
+
+	account := &model.Account{
+		Name:     input.Name,
+		Password: string(hashed),
+	}
+
+	id, err := srv.accountRepository.Insert(account, nil)
+	if err != nil {
+		if column, ok := GetConflictColumn(err); ok {
+			return dto.AccountPK{}, errs.NewConflictError(column)
+		}
+		logger.Error(err.Error())
+		return dto.AccountPK{}, errs.NewUnexpectedError(err.Error())
+	}
+
+	return dto.AccountPK{Id: id}, nil
+}
+
+func (srv *accountService) GenerateJwtPayload(input dto.AccountPK) (jwt.Payload, error) {
+	account, err := srv.accountRepository.GetOne(&model.Account{Id: input.Id})
+	if err != nil {
+		logger.Error(err.Error())
+		return jwt.Payload{}, errs.NewUnexpectedError(err.Error())
+	}
+
+	cc := jwt.CustomClaims{
+		AccountId:   account.Id,
+		AccountName: account.Name,
+	}
+	return jwt.NewPayload(cc), nil
+}
