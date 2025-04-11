570b91baac34f28b63cd2a0e01fc507dd5f2e63d
kodaimura <kodaimura123@gmail.com>
2025-04-11 22:44:03 +0900

    gorm:"primaryKey;autoIncrement"

--- internal/model/account.go
+++ internal/model/account.go
@@ -1,7 +1,7 @@
 package model
 
 type Account struct {
-	AccountId       int    `db:"account_id" json:"account_id"`
+	AccountId       int    `db:"account_id" json:"account_id" gorm:"primaryKey;autoIncrement"`
 	AccountName     string `db:"account_name" json:"account_name"`
 	AccountPassword string `db:"account_password" json:"account_password"`
 	CreatedAt       string `db:"created_at" json:"created_at" gorm:"-"`
